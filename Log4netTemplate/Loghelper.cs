9f66da50e4a2a4286e2605e3a7d57acfd19456f7
yffu <1425866073@qq.com>
2021-08-25 11:12:48 +0800

    初始化

---
+++ Log4netTemplate/Loghelper.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netTemplate
+{
+    class Loghelper
+    {
+        public static bool ReadLog4netConfig()
+        {
+            try
+            {
+                log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(".\\log4net.config"));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static void Outputlog()
+        {
+            Type type = MethodBase.GetCurrentMethod().DeclaringType;
+            ILog m_log = LogManager.GetLogger(type);
+            m_log.Debug("这是一个Debug日志");
+            m_log.Info("这是一个Info日志");
+            m_log.Warn("这是一个Warn日志");
+            m_log.Error("这是一个Error日志");
+            m_log.Fatal("这是一个Fatal日志");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Log4netTemplate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loghelper.ReadLog4netConfig();
+            Loghelper.Outputlog();
+        }
+    }
+}
